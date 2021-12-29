14a18a4cdb205f518e6d75d42a01aaa036c5807a
kulbokasr <kulbokasr@gmail.com>
2021-12-29 18:08:16 +0200

    registracijosapp begining

---
+++ EfCore/RegistracijosApp/RegistracijosApp/Models/Pozymis.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistracijosApp.Models
+{
+    public class Pozymis : Entity
+    {
+        public Reiksme Reiksme { get; set; }
+        public int ReiksmeId { get; set; }
+    }
+}
