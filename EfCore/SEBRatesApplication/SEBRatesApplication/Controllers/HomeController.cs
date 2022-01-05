fc3e23448d3c17ed32c9f7692deb9e0f5c7298a9
kulbokasr <kulbokasr@gmail.com>
2022-01-05 19:56:14 +0200

    SEBRateCalcInit

---
+++ EfCore/SEBRatesApplication/SEBRatesApplication/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SEBRatesApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SEBRatesApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculations(string datestring)
+        {
+            XElement xelement = XElement.Load("http://www.lb.lt/webservices/ExchangeRates/ExchangeRates.asmx/getExchangeRatesByDate?Date=20000105");
+            IEnumerable<XElement> exchangeRates = xelement.Elements();
+            List<Rate> rates = new List<Rate>();
+            foreach (var item in exchangeRates)
+            {
+                Rate rate = new Rate();
+                rate.Date = item.Element("date").Value;
+                rate.Currency = item.Element("currency").Value;
+                rate.Quantity = Int32.Parse(item.Element("quantity").Value);
+                rate.ExRate = item.Element("rate").Value;
+                rate.Unit = item.Element("unit").Value;
+                rates.Add(rate);
+            }
+            return View(rates);
+        }
+
+            public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EfCore/SEBRatesApplication/SEBRatesApplication/Models/Rate.cs
@@ -0,0 +1,11 @@
+﻿namespace SEBRatesApplication.Models
+{
+    public class Rate
+    {
+        public string Date { get; set; }
+        public string Currency { get; set; }
+        public int Quantity { get; set; }
+        public string ExRate { get; set; }
+        public string Unit { get; set; }
+    }
+}
