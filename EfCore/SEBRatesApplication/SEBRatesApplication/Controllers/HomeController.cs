210695a2d9ced5a5fc790130e7c49f3fa64b0542
kulbokasr <kulbokasr@gmail.com>
2022-01-06 18:42:54 +0200

    SEBRatesworking, need small changes

--- EfCore/SEBRatesApplication/SEBRatesApplication/Controllers/HomeController.cs
+++ EfCore/SEBRatesApplication/SEBRatesApplication/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using SEBRatesApplication.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -25,23 +26,55 @@ namespace SEBRatesApplication.Controllers
             return View(rate);
         }
         
-        public IActionResult Calculations(string datestring)
+        public IActionResult Calculations(DateTime datestring)
         {
-            XElement xelement = XElement.Load("http://www.lb.lt/webservices/ExchangeRates/ExchangeRates.asmx/getExchangeRatesByDate?Date=20000105");
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+            provider.NumberGroupSeparator = ",";
+            var datestringchosen = datestring.ToShortDateString().Replace("-", "");
+            var datestringnext = datestring.AddDays(-1).ToShortDateString().Replace("-", "");
+            string linkchosen = "http://www.lb.lt/webservices/ExchangeRates/ExchangeRates.asmx/getExchangeRatesByDate?Date=" + datestringchosen;
+            string linknext = "http://www.lb.lt/webservices/ExchangeRates/ExchangeRates.asmx/getExchangeRatesByDate?Date=" + datestringnext;
+
+            XElement xelement = XElement.Load(linkchosen);
             IEnumerable<XElement> exchangeRates = xelement.Elements();
-            List<Rate> rates = new List<Rate>();
+            List<Rate> rateschosen = new List<Rate>();
             foreach (var item in exchangeRates)
             {
                 Rate rate = new Rate();
-                //rate.DateChosen = datestring;
+                rate.DateChosen = datestringchosen;
                 rate.Date = item.Element("date").Value;
                 rate.Currency = item.Element("currency").Value;
                 rate.Quantity = Int32.Parse(item.Element("quantity").Value);
-                rate.ExRate = item.Element("rate").Value;
+                rate.ExRate = Convert.ToDouble(item.Element("rate").Value, provider);
                 rate.Unit = item.Element("unit").Value;
-                rates.Add(rate);
+                rateschosen.Add(rate);
             }
-            return View(rates);
+
+            XElement xelement1 = XElement.Load(linknext);
+            IEnumerable<XElement> exchangeRates1 = xelement1.Elements();
+            List<Rate> ratesnext = new List<Rate>();
+            foreach (var item in exchangeRates1)
+            {
+                Rate rate = new Rate();
+                rate.DateChosen = datestringchosen;
+                rate.Date = item.Element("date").Value;
+                rate.Currency = item.Element("currency").Value;
+                rate.Quantity = Int32.Parse(item.Element("quantity").Value);
+                rate.ExRate = Convert.ToDouble(item.Element("rate").Value, provider);
+                rate.Unit = item.Element("unit").Value;
+                ratesnext.Add(rate);
+            }
+
+            for (int i = 0; i < rateschosen.Count; i++)
+            {
+                rateschosen[i].Difference = rateschosen[i].ExRate - ratesnext[i].ExRate;
+            }
+
+            List<Rate> orderedRates = new List<Rate>();
+            orderedRates = rateschosen.OrderByDescending(o => o.Difference).ToList();
+
+            return View(orderedRates);
         }
 
             public IActionResult Privacy()
--- EfCore/SEBRatesApplication/SEBRatesApplication/Models/Rate.cs
+++ EfCore/SEBRatesApplication/SEBRatesApplication/Models/Rate.cs
@@ -1,12 +1,15 @@
-﻿namespace SEBRatesApplication.Models
+﻿using System;
+
+namespace SEBRatesApplication.Models
 {
     public class Rate
     {
         public string Date { get; set; }
         public string Currency { get; set; }
         public int Quantity { get; set; }
-        public string ExRate { get; set; }
+        public double ExRate { get; set; }
         public string Unit { get; set; }
         public string DateChosen { get; set; }
+        public double Difference { get; set; }
     }
 }
