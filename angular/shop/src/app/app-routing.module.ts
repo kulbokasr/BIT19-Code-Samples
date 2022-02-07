ba56b1cdf06fc192c3bedd32199d89e2d21f700c
kulbokasr <kulbokasr@gmail.com>
2022-02-07 21:37:29 +0200

    shop CRD

---
+++ angular/shop/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { ShopListComponent } from './components/shop-list/shop-list.component';
+import { ShopCreateComponent } from './components/shop-create/shop-create.component';
+
+const routes: Routes = [
+  { path: 'shop-create', component: ShopCreateComponent},
+  { path: 'shop-list', component: ShopListComponent},
+  { path: '',   redirectTo: '/shop-list', pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
